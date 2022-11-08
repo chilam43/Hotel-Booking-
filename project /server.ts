5c968b66fe61af691b591053f72fbc5bf2a77e57
Donny <donnytkf@gmail.com>
2022-11-08 18:43:19 +0800

    change name of index

--- project /server.ts
+++ project /server.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { print } from "listening-on";
+// import { isRedirect } from "node-fetch";
 
 let app = express();
 
 app.use(express.static("public"));
-
-app.post("/landing", (req, res) => {});
 
 let port = 8030;
 app.listen(port, () => {
