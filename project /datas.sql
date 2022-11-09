0fa5f1f0edfa73a7ec92f006cd6e2e54756f2702
lam <chi43@hotmail.com>
2022-11-09 19:01:30 +0800

    add data

--- project /datas.sql
+++ project /datas.sql
@@ -43,3 +43,114 @@ CREATE TABLE users(
 --     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 --     updated_at TIMESTAMP
 -- );
+INSERT INTO type (name, price)
+VALUES ('A', 500),
+    ('B', 400),
+    ('C', 300),
+    ('D', 200);
+INSERT INTO room (room_number, type_id, floor)
+VALUES (101, 4, 1),
+    (102, 4, 1),
+    (103, 4, 1),
+    (104, 4, 1),
+    (201, 3, 2),
+    (202, 3, 2),
+    (203, 3, 2),
+    (301, 2, 3),
+    (302, 2, 3),
+    (401, 1, 4);
+SELECT *
+FROM room
+    JOIN type on room.type_id = type.id;
+TRUNCATE TABLE type RESTART IDENTITY;
+ALTER SEQUENCE type_id_seq RESTART 1;
+INSERT into users(title, name, email, password)
+VALUES ('MS', 'Alice', 'alice@gmail.com', 'alice'),
+    ('MR', 'Bob', 'bob@gmail.com', 'bob'),
+    ('MR', 'Cat', 'cat@gmail.com', 'cat'),
+    ('MR', 'David', 'david@gmail.com', 'david'),
+    ('MS', 'Eve', 'eve@gmail.com', '');
+INSERT INTO booking_record (
+        room_id,
+        check_in_data,
+        check_out_data,
+        user_id,
+        payment_time,
+        lock_time,
+        confirm_time,
+        cancel_time,
+        ref_number,
+        final_price
+    )
+VALUES (
+        1,
+        '2022-11-09',
+        '2022-11-10',
+        1,
+        '2022-11-01 10:23:54',
+        '2022-11-01 10:30:54',
+        '2022-11-01 10:31:54',
+        null,
+        '11111111',
+        500
+    ),
+    (
+        2,
+        '2022-11-08',
+        '2022-11-11',
+        2,
+        '2022-11-01 10:23:54',
+        '2022-11-01 10:30:54',
+        '2022-11-01 10:31:54',
+        null,
+        '22222222',
+        500
+    ),
+    (
+        10,
+        '2022-11-10',
+        '2022-11-15',
+        3,
+        '2022-11-01 10:23:54',
+        '2022-11-01 10:30:54',
+        '2022-11-01 10:31:54',
+        null,
+        '33333333',
+        500
+    ),
+    (
+        10,
+        '2022-11-10',
+        '2022-11-15',
+        3,
+        '2022-11-01 10:23:54',
+        '2022-11-01 10:30:54',
+        '2022-11-01 10:31:54',
+        '2022-11-02 10:31:54',
+        '44444444',
+        500
+    ),
+    (
+        10,
+        '2022-11-10',
+        '2022-11-15',
+        3,
+        null,
+        '2022-11-01 10:30:54',
+        null,
+        null,
+        '55555555',
+        500
+    ),
+    (
+        10,
+        '2022-11-10',
+        '2022-11-15',
+        3,
+        '2022-11-01 10:23:54',
+        '2022-11-01 10:30:54',
+        null,
+        null,
+        '66666666',
+        500
+    );
