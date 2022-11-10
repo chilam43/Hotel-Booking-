97cd92c61a0883e9d218c6be47462912f742ce9a
Leslie Chunge <lesliecheung0821e@gmail.com>
2022-11-10 16:06:06 +0800

    update submit button and search function

--- project /router/landingRouter.ts
+++ project /router/landingRouter.ts
@@ -6,15 +6,32 @@ export let landing = express.Router();
 
 landing.post("/search-room", async (req, res) => {
 
-    await client.query(/* sql */ `SELECT room.*FROM room
-    WHERE id NOT IN (
-            SELECT room_id
-            FROM booking_record
-            WHERE cancel_time is NOT NULL
-                AND NOT (
-                    check_in_date >= '2022-11-15'::date
-                    OR check_out_date <= '2022-11-10'::date
-                )
-                AND CURRENT_TIMESTAMP > lock_time
-        )`)
+    const { checkIn, checkOut } = req.body;
+    console.log({ checkIn, checkOut });
+
+    try {
+        let data = await client.query(/* sql */ `
+        SELECT room.*
+        FROM room
+        WHERE id NOT IN (
+                SELECT room_id
+                FROM booking_record
+                WHERE cancel_time is NOT NULL
+                    AND NOT (
+                        check_in_date >= $1::date
+                        OR check_out_date <= $2::date
+                    )
+                    AND CURRENT_TIMESTAMP > lock_time
+        )`
+            , [checkIn, checkOut])
+
+        console.log(data.rows);
+
+        res.json({ data: data.rows })
+
+    } catch (error) {
+        console.log(error);
+
+        res.json({ data: [] })
+    }
 })
