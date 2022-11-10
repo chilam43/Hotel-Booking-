ef848cdac75199f3ef504cafafb0e5b48bcbd44e
Donny <donnytkf@gmail.com>
2022-11-10 15:45:43 +0800

    update usersRouter

--- project /router/usersRouter.ts
+++ project /router/usersRouter.ts
@@ -1,42 +1,39 @@
 import express from "express";
-import jsonfile from "jsonfile";
-import path from "path";
+// import jsonfile from "jsonfile";
+// import path from "path";
 import { client } from "../db";
 
 export const userRoutes = express.Router();
 
-type User = {
-  username: string;
-  password: string;
-};
-
-let file = path.resolve("users.json");
-let users: User[] = jsonfile.readFileSync(file);
+// type User = {
+//   username: string;
+//   password: string;
+// };
 
 // login
-userRoutes.post("/login", (req, res) => {
-  let { username, password } = req.body;
+// userRoutes.post("/login", (req, res) => {
+//   let { username, password } = req.body;
 
-  if (!username) {
-    res.status(400);
-    res.end("Missing username");
-    return;
-  }
-  if (!password) {
-    res.status(400);
-    res.end("Missing password");
-    return;
-  }
-  for (let user of users) {
-    if (user.username == username && user.password == password) {
-      res.status(200);
-      res.end("login successful");
-      return;
-    }
-  }
-  res.status(401);
-  res.end("Wrong username or password");
-});
+//   if (!username) {
+//     res.status(400);
+//     res.end("Missing username");
+//     return;
+//   }
+//   if (!password) {
+//     res.status(400);
+//     res.end("Missing password");
+//     return;
+//   }
+//   for (let user of users) {
+//     if (user.username == username && user.password == password) {
+//       res.status(200);
+//       res.end("login successful");
+//       return;
+//     }
+//   }
+//   res.status(401);
+//   res.end("Wrong username or password");
+// });
 
 // register
 userRoutes.post("/register", async (req, res) => {
