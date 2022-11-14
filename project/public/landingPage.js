65f068627b87fde8cfa76569c9e258cd41eb26bd
Leslie Chunge <lesliecheung0821e@gmail.com>
2022-11-14 10:59:41 +0800

    backup data

--- project/public/landingPage.js
+++ project/public/landingPage.js
@@ -82,21 +82,6 @@ document.addEventListener("DOMContentLoaded", function () {
     selectable: true,
 
     dateClick: function (info) {
-      // if (check == true) {
-      //     document.getElementById("check").value = info.dateStr;
-      //     check = !true;
-      // }
-      // if (check == true) {
-      //     let a = document.getElementById("check-out").value = info.dateStr;
-      //     check = !true;
-      // }
-
-      // if (checkIn.value && checkOut.value && checkIn.value > checkOut.value) {
-
-      //     alert("Wrong");
-      //     return;
-      //     // todo replace
-      // }
 
       console.log(info.dateStr);
 
@@ -138,34 +123,34 @@ document.addEventListener("click", (event) => {
 
 // submit
 document
-    .querySelector("#form-check-availability")
-    .addEventListener("submit", async function (event) {
-        event.preventDefault();
-        const form = event.target;
-        const formObject = {};
-        formObject["checkIn"] = form.checkIn.value;
-        formObject["checkOut"] = form.checkOut.value;
-        console.log(formObject)
-        console.log("done");
-        const res = await fetch("/search-room", { // change /register
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formObject),
-        });
-        const result = await res.json();
-        console.log(result);
-        // document.querySelector("#contact-result").textContent = result;
+  .querySelector("#form-check-availability")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const form = event.target;
+    const formObject = {};
+    formObject["checkIn"] = form.checkIn.value;
+    formObject["checkOut"] = form.checkOut.value;
+    console.log(formObject)
+    console.log("done");
+    const res = await fetch("/search-room", { // change /register
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formObject),
     });
+    const result = await res.json();
+    console.log(result);
+    // document.querySelector("#contact-result").textContent = result;
+  });
 
 checkAvailability.addEventListener('click', async function () {
-    // // window.location = path.join(__dirname, "/select_room.html")
-    // const checkAva = await fetch("/search", {
-    //     method: "POST",
-    // });
+  // // window.location = path.join(__dirname, "/select_room.html")
+  // const checkAva = await fetch("/search", {
+  //     method: "POST",
+  // });
 
-    location.href = "/select_room.html"
+  location.href = "/select_room.html"
 })
 
 // sweetAlert
