f0e38cb36249892b20526b8579e1937a9356c6fa
Donny <donnytkf@gmail.com>
2022-11-18 12:02:49 +0800

    connect send emil with payment

--- project/router/send_email.ts
+++ project/router/send_email.ts
@@ -1,8 +1,14 @@
-import express from "express";
 const nodemailer = require("nodemailer");
 // import nodemailer from "nodemailer";
+import { client } from "../db";
 
-export async function sendEmailToUsers() {
+export async function sendEmailToUsers(ref_num: string) {
+  let paymentRecord = await client.query(
+    /* sql */ `select ref_number from payment_history where ref_number = $1`,
+    [ref_num]
+  );
+
+  console.log("paymentRecord:", paymentRecord);
 
   let transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com", // hostname
@@ -21,7 +27,7 @@ export async function sendEmailToUsers() {
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: "hotelbooking6969@outlook.com", // sender address
-    to: "donnytkf@gmail.com", // list of receivers
+    to: paymentRecord.rows[0].email, // list of receivers
     subject: "Hello ✔", // Subject line
     text: "69?", // plain text body
     html: "<b>Hello world?</b>", // html body
@@ -29,21 +35,3 @@ export async function sendEmailToUsers() {
 
   console.log("Message sent: %s", info.messageId);
 }
-
-export const sendemailRountes = express.Router();
-
-sendemailRountes.post("/email", async (req, res) => {
-  // async..await is not allowed in global scope, must use a wrapper
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  // let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-  res.json({});
-});
