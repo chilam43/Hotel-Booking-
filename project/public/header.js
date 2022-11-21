9880c1951f56a9bef31d9422f7e50a148bb80311
Donny <donnytkf@gmail.com>
2022-11-21 11:17:31 +0800

    add sweet alert

--- project/public/header.js
+++ project/public/header.js
@@ -21,19 +21,68 @@ window.onload = async () => {
         // F5
       });
   } else {
-
     // document.querySelector("#loginLogout").textContent = "Login";
     // document.querySelector("#register").textContent = "Sign-up";
 
     document.querySelector("#loginLogout").textContent = "Login";
+    document
+      .querySelector("#loginLogout")
+      .addEventListener("click", async (e) => {
+        callAlert();
+      });
     document.querySelector("#register").textContent = "Sign-up";
     document
       .querySelector("#userBookingRecordLink")
       .addEventListener("click", function () {
         location.href = "/register.html";
       });
-
   }
 };
+
+async function callAlert() {
+  Swal.fire({
+    title: "Login Form",
+    html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
+  <input type="password" id="password" class="swal2-input" placeholder="Password">`,
+    confirmButtonText: "Sign in",
+    focusConfirm: false,
+    preConfirm: () => {
+      const login = Swal.getPopup().querySelector("#login").value;
+      const password = Swal.getPopup().querySelector("#password").value;
+      if (!login || !password) {
+        Swal.showValidationMessage(`Please enter login and password`);
+      }
+      return { login: login, password: password };
+    },
+  }).then(async (result) => {
+    console.log(result);
+    const formObject = {};
+    formObject["username"] = result.value.login;
+    formObject["password"] = result.value.password;
+
+    const res = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formObject),
+    });
+
+    const serverResult = await res.json();
+    console.log(serverResult);
+    // console.log(result);
+    // if (result.status) {
+    //   alert(result.msg);
+    //   location.href = "/index.html";
+    // } else {
+    //   alert(result.msg);
+    // }
+    if (res.status == 200) {
+      location.href = "index.html";
+    } else {
+      alert(serverResult.msg);
+    }
+  });
+}
 
 //yo yo
